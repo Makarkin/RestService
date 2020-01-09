e903fddc71fafb2fd6ecfce35d3669c0de8de278
Stanislav Makarkin <Stanislav_Makarkin@epam.com>
2020-01-09 12:36:24 +0400

    push1

--- src/main/java/hello/UserRepository.java
+++ src/main/java/hello/UserRepository.java
@@ -3,6 +3,7 @@ package hello;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RestResource;
 
+
 @RestResource(exported = false)
 public interface UserRepository extends JpaRepository<User, String> {
 }
